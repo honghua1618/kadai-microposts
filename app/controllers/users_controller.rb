2181023d27e113c46ecfc281bfa19ba2e1a02b22
honghua1618 <renn_renn1@yahoo.co.jp>
2019-08-18 12:45:05 +0000

    herok commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
-
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -26,7 +25,7 @@ class UsersController < ApplicationController
       render :new
     end
   end
-
+  
   def followings
     @user = User.find(params[:id])
     @followings = @user.followings.page(params[:page])
@@ -38,6 +37,13 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  
+  def favorites
+    @user = User.find(params[:id])
+    @favorites = @user.fav_posts.page(params[:page])
+    counts(@user)
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'toppages#index'
-
+    root to: 'toppages#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
@@ -10,12 +9,11 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
+      get :favorites
     end
-    # collection do
-    #   get :search
-    # end
   end
-  
+
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
